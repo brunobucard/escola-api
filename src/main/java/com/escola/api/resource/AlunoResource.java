2c64dcf38a1df75d2daefb222aa0dab5a75ffd51
brunobucard <brunobucard@gmail.com>
2018-10-17 22:50:42 -0300

    Aluno novos recursos

--- src/main/java/com/escola/api/resource/AlunoResource.java
+++ src/main/java/com/escola/api/resource/AlunoResource.java
@@ -10,17 +10,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escola.api.event.RecursoCriadoEvent;
 import com.escola.api.model.Aluno;
 import com.escola.api.repository.AlunoRepository;
+import com.escola.api.service.AlunoService;
 
 @RestController
 @RequestMapping("/alunos")
@@ -28,6 +32,9 @@ public class AlunoResource {
 	
 	@Autowired
 	private AlunoRepository alunoRepository;
+	
+	@Autowired
+	private AlunoService alunoService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -47,10 +54,24 @@ public class AlunoResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_ALUNO') and #oauth2.hasScope('read')")
 	public ResponseEntity<Aluno> buscarPeloCodigo(@PathVariable Long codigo) {
 		Aluno aluno = alunoRepository.findOne(codigo);
 		return aluno != null ? ResponseEntity.ok(aluno) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_ALUNO') and #oauth2.hasScope('write')")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		alunoRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_ALUNO') and #oauth2.hasScope('write')")
+	public ResponseEntity<Aluno> atualizar(@PathVariable Long codigo, @Valid @RequestBody Aluno aluno) {
+		Aluno alunoSalvo = alunoService.atualizar(codigo, aluno);
+		return ResponseEntity.ok(alunoSalvo);
 	}
 
 }
---
+++ src/main/java/com/escola/api/service/AlunoService.java
@@ -0,0 +1,32 @@
+package com.escola.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.escola.api.model.Aluno;
+import com.escola.api.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	public Aluno atualizar(Long codigo, Aluno aluno) {
+		Aluno alunoSalvo = buscarAlunoPeloCodigo(codigo);
+		BeanUtils.copyProperties(aluno, alunoSalvo, "codigo");
+		return alunoRepository.save(alunoSalvo);
+	}
+	
+	private Aluno buscarAlunoPeloCodigo(Long codigo) {
+		Aluno alunoSalvo = alunoRepository.findOne(codigo);
+		if (alunoSalvo == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return alunoSalvo;
+	}
+	
+
+}
