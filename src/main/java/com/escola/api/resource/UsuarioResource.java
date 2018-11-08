920c26f9bd83608e0e986f89a0aebe8fdde1f656
brunobucard <brunobucard@gmail.com>
2018-11-08 00:18:42 -0200

    UsuarioResource Delete

--- src/main/java/com/escola/api/resource/UsuarioResource.java
+++ src/main/java/com/escola/api/resource/UsuarioResource.java
@@ -10,10 +10,13 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escola.api.event.RecursoCriadoEvent;
@@ -49,4 +52,10 @@ public class UsuarioResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
 	}
 
+	@DeleteMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_USUARIO') and #oauth2.hasScope('write')")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		usuarioRepository.delete(codigo);
+	}
 }
