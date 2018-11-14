9d5f3574d8e91c9c4219520c0b544f2de0120335
brunobucard <brunobucard@gmail.com>
2018-11-14 17:31:42 -0200

    Models Turma Sala Periodo

---
+++ src/main/java/com/escola/api/model/Turma.java
@@ -0,0 +1,121 @@
+package com.escola.api.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="turma")
+public class Turma {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	private String turma;
+	
+	private Integer vaga;
+	
+	@Enumerated(EnumType.STRING)
+	private Periodo periodo;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_sala")
+	private Sala sala;
+	
+	@ManyToMany
+	@JoinTable(name="turma_funcionario", joinColumns = @JoinColumn(name="codigo_turma")
+	, inverseJoinColumns = @JoinColumn(name="codigo_funcionario"))
+	private List<Funcionario> funcionarios;
+
+	
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+	public Integer getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Integer vaga) {
+		this.vaga = vaga;
+	}
+
+	public Periodo getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(Periodo periodo) {
+		this.periodo = periodo;
+	}
+	
+	
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Turma other = (Turma) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/com/escola/api/model/Turma_.java
@@ -0,0 +1,20 @@
+package com.escola.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Turma.class)
+public abstract class Turma_ {
+
+	public static volatile SingularAttribute<Turma, Long> codigo;
+	public static volatile SingularAttribute<Turma, Integer> vaga;
+	public static volatile SingularAttribute<Turma, Periodo> periodo;
+	public static volatile SingularAttribute<Turma, Sala> sala;
+	public static volatile ListAttribute<Turma, Funcionario> funcionarios;
+	public static volatile SingularAttribute<Turma, String> turma;
+
+}
+
