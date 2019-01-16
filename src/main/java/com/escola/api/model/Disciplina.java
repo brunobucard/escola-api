2fa5150ab363987ced095dc88816b9b1a00183ff
brunobucard <brunobucard@gmail.com>
2019-01-15 22:49:08 -0200

    disciplina model

---
+++ src/main/java/com/escola/api/model/Disciplina.java
@@ -0,0 +1,72 @@
+package com.escola.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="disciplina")
+public class Disciplina {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	private String descricao;
+	
+	private String abreviacao;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getAbreviacao() {
+		return abreviacao;
+	}
+
+	public void setAbreviacao(String abreviacao) {
+		this.abreviacao = abreviacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/escola/api/model/Disciplina_.java
@@ -0,0 +1,16 @@
+package com.escola.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Disciplina.class)
+public abstract class Disciplina_ {
+
+	public static volatile SingularAttribute<Disciplina, Long> codigo;
+	public static volatile SingularAttribute<Disciplina, String> abreviacao;
+	public static volatile SingularAttribute<Disciplina, String> descricao;
+
+}
+
